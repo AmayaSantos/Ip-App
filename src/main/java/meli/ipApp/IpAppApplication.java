9150d9cd852b7b61ed26262b24e8674a426cb4ed
emanuel.amaya <emanuelamaya@julasoft.com>
2024-10-05 13:15:07 -0700

    init commit

---
+++ src/main/java/meli/ipApp/IpAppApplication.java
@@ -0,0 +1,14 @@
+package meli.ipApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@EnableSwagger2
+public class IpAppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(IpAppApplication.class, args);
+  }
+
+}
