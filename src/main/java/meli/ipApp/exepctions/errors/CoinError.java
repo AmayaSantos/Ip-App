ac571808e229e8226fe6c3db2210b9bbd6e159d3
emanuel.amaya <emanuelamaya@julasoft.com>
2024-10-06 17:04:08 -0700

    add errors BASE_NOT_FOUND and REQUIRED_COIN_DOLLAR_EQUIVALENT in CoinError

--- src/main/java/meli/ipApp/exepctions/errors/CoinError.java
+++ src/main/java/meli/ipApp/exepctions/errors/CoinError.java
@@ -6,7 +6,8 @@ import meli.ipApp.exepctions.AppError;
 public enum CoinError implements AppError {
 
   EXTERNAL_COIN_APP_ERROR("External coin app error", "20"),
-
+  BASE_NOT_FOUND("Base not found", "21"),
+  REQUIRED_COIN_DOLLAR_EQUIVALENT("Required coin dollar equivalent", "22"),
   ;
 
   private final String message;
