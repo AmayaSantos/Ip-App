fa3a141530fe3e6acad4b4d4828d9db5a2c26b57
emanuel.amaya <emanuelamaya@julasoft.com>
2024-10-06 17:04:28 -0700

    add error INTERNAL_ERROR_IN_GET_COUNTRY_INFO in CountryError

--- src/main/java/meli/ipApp/exepctions/errors/CountryError.java
+++ src/main/java/meli/ipApp/exepctions/errors/CountryError.java
@@ -7,6 +7,7 @@ public enum CountryError implements AppError {
 
   EXTERNAL_COUNTRY_APP_ERROR("External country app error", "15"),
   COUNTRY_BASE_NOT_FOUND("Base country location not found", "16"),
+  INTERNAL_ERROR_IN_GET_COUNTRY_INFO("Error interno al obtener country info", "17"),
 
   ;
 
