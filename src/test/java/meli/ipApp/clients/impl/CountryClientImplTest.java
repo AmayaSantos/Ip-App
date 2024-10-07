ddae633a6d25ec063bbbc255c5dba0569a8aa0dd
emanuel.amaya <emanuelamaya@julasoft.com>
2024-10-07 15:38:49 -0700

    CountryClientImplTest with TestConfig

--- src/test/java/meli/ipApp/clients/impl/CountryClientImplTest.java
+++ src/test/java/meli/ipApp/clients/impl/CountryClientImplTest.java
@@ -2,11 +2,14 @@ package meli.ipApp.clients.impl;
 
 import static org.junit.Assert.assertNotNull;
 
+import meli.ipApp.mockUtils.WithOutClientsConfiguration.TestConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes = {TestConfig.class,})
 class CountryClientImplTest {
 
   @Autowired
