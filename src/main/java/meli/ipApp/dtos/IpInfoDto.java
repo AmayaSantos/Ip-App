a35bec675df358a7e8c948f88197744c6e271b99
emanuel.amaya <emanuelamaya@julasoft.com>
2024-10-06 17:02:27 -0700

    fix countryCode in IpInfoDto

--- src/main/java/meli/ipApp/dtos/IpInfoDto.java
+++ src/main/java/meli/ipApp/dtos/IpInfoDto.java
@@ -1,17 +1,23 @@
 package meli.ipApp.dtos;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class IpInfoDto {
 
   private String ip;
+  @JsonProperty("country_code")
   private String countryCode;
   private CountryInfoDto countryInfoDto;
+  private Double longitude;
+  private Double latitude;
 
   public void setTime() {
     countryInfoDto.setTime();
