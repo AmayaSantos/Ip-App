317105fa6368785ebe851dde5acfbad6d0d76979
emanuel.amaya <emanuelamaya@julasoft.com>
2024-10-06 17:03:39 -0700

    add getSupportedSymbols in CoinClient

--- src/main/java/meli/ipApp/clients/CoinClient.java
+++ src/main/java/meli/ipApp/clients/CoinClient.java
@@ -1,8 +1,11 @@
 package meli.ipApp.clients;
 
 import meli.ipApp.dtos.CoinsInfoDto;
+import meli.ipApp.dtos.SymbolsDto;
 
 public interface CoinClient {
 
   CoinsInfoDto getCoinsInfo();
+
+  SymbolsDto getSupportedSymbols();
 }
--- src/main/java/meli/ipApp/clients/impl/CoinClientImpl.java
+++ src/main/java/meli/ipApp/clients/impl/CoinClientImpl.java
@@ -2,6 +2,7 @@ package meli.ipApp.clients.impl;
 
 import meli.ipApp.clients.CoinClient;
 import meli.ipApp.dtos.CoinsInfoDto;
+import meli.ipApp.dtos.SymbolsDto;
 import meli.ipApp.exepctions.AppException;
 import meli.ipApp.exepctions.errors.CoinError;
 import org.slf4j.Logger;
@@ -22,12 +23,18 @@ public class CoinClientImpl implements CoinClient {
   private final static String ACCESS_KEY = "access_key";
   private final Logger logger = LoggerFactory.getLogger(CoinClientImpl.class);
   private final String coinHost;
+  private final String pathToGetCoins;
+  private final String pathToGetSymbols;
   private final String accessKey;
 
   public CoinClientImpl(
       @Value("${external.coin.host}") String coinHost,
+      @Value("${external.coin.pathToGetCoins}") String pathToGetCoins,
+      @Value("${external.coin.pathToGetSymbols}") String pathToGetSymbols,
       @Value("${external.coin.accessKey}") String accessKey) {
     this.coinHost = coinHost;
+    this.pathToGetSymbols = pathToGetSymbols;
+    this.pathToGetCoins = pathToGetCoins;
     this.accessKey = accessKey;
     logger.debug("created");
   }
@@ -36,15 +43,29 @@ public class CoinClientImpl implements CoinClient {
   public CoinsInfoDto getCoinsInfo() {
     logger.info("try to get info");
     try {
-      return callGet(createCoinUrl());
+      String coinUrl = createCoinUrl(pathToGetCoins);
+      return callGet(coinUrl);
     } catch (Exception e) {
       logger.error("can`t get info");
       throw new AppException(CoinError.EXTERNAL_COIN_APP_ERROR, HttpStatus.FAILED_DEPENDENCY);
     }
   }
 
-  private String createCoinUrl() {
+  @Override
+  public SymbolsDto getSupportedSymbols() {
+    logger.info("try to get info");
+    try {
+      String coinUrl = createCoinUrl(pathToGetSymbols);
+      return callGetSymbols(coinUrl);
+    } catch (Exception e) {
+      logger.error("can`t get info");
+      throw new AppException(CoinError.EXTERNAL_COIN_APP_ERROR, HttpStatus.FAILED_DEPENDENCY);
+    }
+  }
+
+  private String createCoinUrl(String path) {
     return UriComponentsBuilder.fromUriString(coinHost)
+        .path(path)
         .queryParam(ACCESS_KEY, accessKey)
         .build().toUriString();
   }
@@ -66,4 +87,20 @@ public class CoinClientImpl implements CoinClient {
     }
   }
 
+  private SymbolsDto callGetSymbols(String apiUrl) {
+    logger.info("called to get info");
+
+    ResponseEntity<SymbolsDto> responseEntity =
+        new RestTemplate().exchange(
+            apiUrl,
+            HttpMethod.GET,
+            new HttpEntity<>(new HttpHeaders()),
+            SymbolsDto.class);
+
+    if (HttpStatus.OK.equals(responseEntity.getStatusCode())) {
+      return responseEntity.getBody();
+    } else {
+      throw new AppException(CoinError.EXTERNAL_COIN_APP_ERROR, HttpStatus.FAILED_DEPENDENCY);
+    }
+  }
 }
