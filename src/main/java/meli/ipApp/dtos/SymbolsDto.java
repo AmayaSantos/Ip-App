f32984dfb59eeef07fc430c3328d48465df9ecbd
emanuel.amaya <emanuelamaya@julasoft.com>
2024-10-06 17:02:38 -0700

    create SymbolsDto

---
+++ src/main/java/meli/ipApp/dtos/SymbolsDto.java
@@ -0,0 +1,14 @@
+package meli.ipApp.dtos;
+
+import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SymbolsDto {
+
+  private Map<String, String> symbols;
+}
