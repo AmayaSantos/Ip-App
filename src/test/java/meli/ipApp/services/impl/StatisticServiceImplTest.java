7f0c925f663fcffd2ed894e348283f654eb61dd1
emanuel.amaya <emanuelamaya@julasoft.com>
2024-10-06 19:41:57 -0700

    make complex text in StatisticServiceImpl

---
+++ src/test/java/meli/ipApp/services/impl/StatisticServiceImplTest.java
@@ -0,0 +1,43 @@
+package meli.ipApp.services.impl;
+
+import static org.junit.Assert.assertEquals;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import meli.ipApp.dtos.IpInfoDto;
+import meli.ipApp.dtos.StatisticDto;
+import meli.ipApp.mockUtils.WithOutClientsConfiguration.TestConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+@ContextConfiguration(classes = {TestConfig.class,})
+class StatisticServiceImplTest {
+
+  @Autowired
+  private StatisticServiceImpl statisticService;
+
+  @Autowired
+  IpServiceImpl ipService;
+
+  @Test
+  void updateStatisticsWith() {
+    IpInfoDto ipInfoES = ipService.getIpInfo("3.44.196.93");
+    Double distBsAs = ipInfoES.getCountryInfoDto().getDistBsAs();
+
+    IpInfoDto ipInfoAR = ipService.getIpInfo("13.44.196.93");
+    StatisticDto allStatistics = statisticService.getAllStatistics();
+
+    assertEquals(
+        BigDecimal.valueOf(distBsAs).divide(BigDecimal.valueOf(2), RoundingMode.HALF_UP)
+        ,allStatistics.getPromDist());
+
+    assertEquals(ipInfoES.getCountryCode(),allStatistics.getFurthestCountry().getCountry().getAlpha2Code());
+
+    assertEquals(ipInfoAR.getCountryCode(),allStatistics.getClosestCountry().getCountry().getAlpha2Code());
+
+  }
+
+}
